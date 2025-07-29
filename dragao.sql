cb937b5076bfcde11984090326bc0410743ed4d4
Aluno <jeipi7007@gmail.com>
2025-07-29 08:54:17 -0300

    First Commit

---
+++ dragao.sql
@@ -0,0 +1,13 @@
+create table dragoes
+
+use dragoes
+
+CREATE TABLE dragoes (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  nome VARCHAR(100) NOT NULL,
+  tipo VARCHAR(50),
+  habitat VARCHAR(100),
+  nivel INT,
+  raridade VARCHAR(50),
+  imagem_url TEXT
+);
